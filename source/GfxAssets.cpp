3b90aba42c67bc63eb63869f20839af1fb93133a
DESKTOP-58047GN\Maks- <maksimilian.suvajev@gmail.com>
2025-07-22 00:09:50 +0200

    Small refactor

--- source/GfxAssets.cpp
+++ source/GfxAssets.cpp
@@ -3,72 +3,74 @@
 
 namespace gfx {
 
+//Constuctor 
+GfxAssets::GfxAssets()
+{
+    loadShaderPaths();
+}
 
-    //Constuctor 
-    GfxAssets::GfxAssets()
+// Loads shader source file paths into shaderPaths structure (.vs and .fs) found in Shaders/glsl (in current default config)
+// TODO: This should be an interface to a generic file type finder - need to do this before writing a file finder for any other specific format
+// TODO: Integrate standard logging/debug system to replace cout calls
+void GfxAssets::loadShaderPaths()
+{
+    namespace fs = std::filesystem; // declutter code
+
+    fs::path sourcePath = __FILE__; //path of current source file TODO: compile time only right now, might need to be more dynamic in future
+
+    fs::path currentDir = sourcePath.parent_path(); // removes file name from path
+
+    bool shaderProjectFound = false;
+
+    while (currentDir != rootString && !shaderProjectFound) // Searching to root may perhaps be over kill
     {
-        loadShaderPaths();
-    }
+        std::cout << " DEBUG::current directory = " << currentDir.string() << std::endl;
 
-    // Loads shader source file paths into shaderPaths structure (.vs and .fs) found in Shaders/glsl (in current default config)
-    // TODO: This should be an interface to a generic file type finder - need to do this before writing a file finder for any other specific format
-    // TODO: Integrate standard logging/debug system to replace cout calls
-    void GfxAssets::loadShaderPaths()
-    {
-        namespace fs = std::filesystem; // declutter code
+        fs::path checkDir = currentDir / shaderModuleProjectName;
 
-        fs::path sourcePath = __FILE__; //path of current source file TODO: compile time only right now, might need to be more dynamic in future
-
-        fs::path currentDir = sourcePath.parent_path(); // removes file name from path
-
-        while (currentDir != rootString) // Searching to root may perhaps be over kill
+        if(fs::exists(checkDir))
         {
-            std::cout << " DEBUG::current directory = " << currentDir.string() << std::endl;
+            shaderProjectFound = true; // Kill the loop once the Shader project is found.
 
-            fs::path checkDir = currentDir / shaderModuleProjectName;
+            // Should I check for a .git here and check the project? Possibly even check the commit hash?
+            std::cout << "DEBUG::" << shaderModuleProjectName << " directory found." << std::endl;
 
-            if(fs::exists(checkDir))
+            checkDir = checkDir / shaderSourceFolderName; // now look for glsl folder
+
+            if (fs::exists(checkDir))
             {
-                // Should I check for a .git here and check the project? Possibly even check the commit hash?
-                std::cout << "DEBUG::" << shaderModuleProjectName << " directory found." << std::endl;
+                std::cout << "DEBUG::" << shaderSourceFolderName << " directory found." << std::endl;
 
-                checkDir = checkDir / shaderSourceFolderName; // now look for glsl folder
+                std::cout << "DEBUG::Source files found:" << std::endl;
 
-                if (fs::exists(checkDir))
+                for (const auto& file : fs::directory_iterator(checkDir))
                 {
-                    std::cout << "DEBUG::" << shaderSourceFolderName << " directory found." << std::endl;
-
-                    std::cout << "DEBUG::Source files found:" << std::endl;
-
-                    for (const auto& file : fs::directory_iterator(checkDir))
+                    if (file.is_regular_file())
                     {
-                        if (file.is_regular_file())
+                        if (file.path().extension().string() == vertShaderExtension)
                         {
-                            if (file.path().extension().string() == vertShaderExtension)
-                            {
-                                std::cout << "Vertex shader: " << file.path().filename() << std::endl;
-                                shaderPaths.vertexShader = file.path();
-                            }
-                            else if (file.path().extension().string() == fragShaderExtension)
-                            {
-                                std::cout << "Fragment shader: " << file.path().filename() << std::endl;
-                                shaderPaths.fragmentShader = file.path();
-                            }
+                            std::cout << "Vertex shader: " << file.path().filename() << std::endl;
+                            shaderPaths.vertexShader = file.path();
+                        }
+                        else if (file.path().extension().string() == fragShaderExtension)
+                        {
+                            std::cout << "Fragment shader: " << file.path().filename() << std::endl;
+                            shaderPaths.fragmentShader = file.path();
                         }
                     }
                 }
             }
-
-            currentDir = currentDir.parent_path();
         }
-    } 
 
-    void GfxAssets::resetShaderPaths()
-    {
-        shaderPaths.vertexShader = std::filesystem::path(); 
-        shaderPaths.fragmentShader = std::filesystem::path(); 
+        currentDir = currentDir.parent_path();
     }
+} 
 
+void GfxAssets::resetShaderPaths()
+{
+    shaderPaths.vertexShader = std::filesystem::path(); 
+    shaderPaths.fragmentShader = std::filesystem::path(); 
+}
 
 }
 
--- source/GfxAssets.h
+++ source/GfxAssets.h
@@ -1,56 +1,55 @@
 #ifndef GFX_ASSETS_H
 #define GFX_ASSETS_H
 
-	#include "Assets.h"
-	#include <algorithm>
-	#include <ranges>
-	#include <filesystem>
-	#include <iostream>
-	#include <vector>
-	#include <string>
+#include "Assets.h"
+#include <algorithm>
+#include <ranges>
+#include <filesystem>
+#include <iostream>
+#include <vector>
+#include <string>
 
-	namespace gfx {
+namespace gfx {
 
-		#if  defined(_WIN32)
-			inline constexpr char rootString[] =  "C:\\";
-		#elif defined(__unix__) || defined(__APPLE__) // Very system dependant, needs to be tested
-			inline constexpr char rootString[] =  "/";
-		#else
-			inline constexpr char rootString[] =  "C:\\"; // Windows default
-		#endif
+#if  defined(_WIN32)
+	inline constexpr char rootString[] =  "C:\\";
+#elif defined(__unix__) || defined(__APPLE__) // Very system dependant, needs to be tested
+	inline constexpr char rootString[] =  "/";
+#else
+	inline constexpr char rootString[] =  "C:\\"; // Windows default
+#endif
 
-		inline constexpr char shaderModuleProjectName[] = "Shaders"; // TODO: Should be moved externally to project config
-		inline constexpr char shaderSourceFolderName[] = "glsl"; // TODO: same as above
+inline constexpr char shaderModuleProjectName[] = "Shaders"; // TODO: Should be moved externally to project config
+inline constexpr char shaderSourceFolderName[] = "glsl"; // TODO: same as above
 
-		// extensions for file types used TODO: possibly moved to config file later
-   	 	inline constexpr char vertShaderExtension[] = ".vs";
-		inline constexpr char fragShaderExtension[] = ".fs";
+// extensions for file types used TODO: possibly moved to config file later
+inline constexpr char vertShaderExtension[] = ".vs";
+inline constexpr char fragShaderExtension[] = ".fs";
 
-		// Implemented as a singleton, we should only ever need a single instance to pull all of the required assets
-		class GfxAssets : public Assets
+// Implemented as a singleton, we should only ever need a single instance to pull all of the required assets
+class GfxAssets : public Assets
+{
+	public:
+
+		GfxAssets();
+		void resetShaderPaths();
+		void loadShaderPaths();
+		std::filesystem::path getVertShaderPath(){return shaderPaths.vertexShader;};
+		std::filesystem::path getFragShaderPath(){return shaderPaths.fragmentShader;};
+
+	private:
+
+		// Define type to hold shader paths
+		struct ShaderPaths
 		{
-			public:
-
-				GfxAssets();
-				void resetShaderPaths();
-				void loadShaderPaths();
-				std::filesystem::path getVertShaderPath(){return shaderPaths.vertexShader;};
-				std::filesystem::path getFragShaderPath(){return shaderPaths.fragmentShader;};
-
-			private:
-
-				// Define type to hold shader paths
-				struct ShaderPaths
-				{
-					// Single instance of each for now, will possibly need to become vector if multiple shaders become required
-					std::filesystem::path vertexShader;
-					std::filesystem::path fragmentShader;
-				};
-
-				ShaderPaths shaderPaths; 
+			// Single instance of each for now, will possibly need to become vector if multiple shaders become required
+			std::filesystem::path vertexShader;
+			std::filesystem::path fragmentShader;
 		};
-	}
 
+		ShaderPaths shaderPaths; 
+};
+}
 
 #endif
 
